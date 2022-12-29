ae3c821b06bb242a3365198d614a1602f639bbb0
yuyuyu0819 <safariabc.123@gmail.com>
2022-12-29 19:33:59 +0900

    ユーザー登録

--- db/migrate/20221229015352_devise_create_users.rb
+++ db/migrate/20221229015352_devise_create_users.rb
@@ -8,7 +8,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
       t.string :encrypted_password, null: false, default: ""
       t.string :nickname           , null: false
       t.string :first_name         , null: false       
-      t.string :lase_name           ,null: false
+      t.string :last_name           ,null: false
       t.string :first_name_kana   ,  null: false
       t.string :last_name_kana   ,   null: false
       t.date   :birth_day          , null: false
--- db/schema.rb
+++ db/schema.rb
@@ -17,7 +17,7 @@ ActiveRecord::Schema.define(version: 2022_12_29_015352) do
     t.string "encrypted_password", default: "", null: false
     t.string "nickname", null: false
     t.string "first_name", null: false
-    t.string "lase_name", null: false
+    t.string "last_name", null: false
     t.string "first_name_kana", null: false
     t.string "last_name_kana", null: false
     t.date "birth_day", null: false
