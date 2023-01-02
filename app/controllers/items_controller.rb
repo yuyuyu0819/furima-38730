43a019af6874bbab888d26149649e945bf864314
yuyuyu0819 <safariabc.123@gmail.com>
2023-01-02 17:33:05 +0900

    activestrageの導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -7,6 +7,9 @@ class ItemsController < ApplicationController
   def new
   end
 
-
+ private
+  def item_params
+    params.require(:item).permit(:image, :item_title, :item_description, :item_status_id. :item_delivery_charge_id, :prefecture_id, :delivery_date_id, :category_id, :price).merge(user_id: current_user.id)
+  end
  
 end
