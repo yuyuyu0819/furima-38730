a11448f2c30c26bf6596cf8e4d41292da0a250f6
yuyuyu0819 <safariabc.123@gmail.com>
2023-01-06 19:40:37 +0900

    進んだ部分のコメントアウト

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,8 +2,6 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index]
 
   def index
-    @items = Item.includes(:user).order('created_at DESC')
-
   end
 
   def new
